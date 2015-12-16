923c5b3bdd5711763519543fda235e136cfc7f8f
maxali <maxali.abdi@gmail.com>
2015-12-16 22:00:37 +0100

    init

---
+++ xls2json.js
@@ -0,0 +1,44 @@
+
+function xls(filename) {
+	var self = {};
+	var xls = require('xlsjs');
+	self.workbook = null;
+	self.sheet = null;
+
+
+	self.readFile = function(filename) {
+		self.workbook = xls.readFile(filename);		
+		self.sheetNames = self.workbook.SheetNames;
+		console.log('executed;');
+	}
+
+	self.readFile(filename);
+
+	self.selectSheet = function(sheet) {
+		sheet = (typeof sheet == "string" || typeof sheet == "number") ? sheet : 0;
+		self.sheet = self.workbook.Sheets[self.workbook.SheetNames[sheet]];
+		return self;
+	};
+
+	self.toJSON = function(filename, sheetnr) {
+
+		if(filename) self.readFile(filename);
+		if(sheetnr) self.selectSheet(0);
+		if(!self.sheet) self.selectSheet(0);
+
+		var data = xls.utils.sheet_to_json(self.sheet);
+
+		return data;
+	};	
+
+	self.uttak = self.toJSON().filter(function(item, i){
+		return !!item.Uttak;
+	});
+	
+	self.innskudd = self.toJSON().filter(function(item, i){
+		return !!item.Innskudd;
+	})
+	
+	return self;
+}
+module.exports = xls;
